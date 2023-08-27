e16b5a955d5e5c17bdbce9e8288eff6585a1883f
Lorenzo Maturano <maturanolorenzo@gmail.com>
2023-08-27 20:25:29 -0300

    Channels page

--- client/src/lib/index.ts
+++ client/src/lib/index.ts
@@ -1,1 +1,26 @@
-// place files you want to import through the `$lib` alias in this folder.
+export const endpoint = "http://localhost:8000/api";
+
+export const formatSats = (sats: number) =>
+    new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BTC",
+        currencyDisplay: "narrowSymbol",
+        signDisplay: "always",
+        minimumFractionDigits: 8,
+    })
+        .format(sats / 1e8)
+        .replace("BTC", "₿");
+
+export const formatPercent = (number: number): string => {
+    return new Intl.NumberFormat("pt-BR", {
+        style: "percent",
+    }).format(number);
+};
+
+export function formatFee(fee: number): string {
+    return `${fee} sat/vB`;
+}
+
+export function formatPayreq(payreq: string): string {
+    return payreq.slice(0, 10) + "..." + payreq.slice(-10);
+}
---
+++ client/src/routes/channels/BalanceTable.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+    import DataTable, { Body, Cell, Head, Row } from "@smui/data-table";
+    import CellWithInfo from "../CellWithInfo.svelte";
+    import { formatSats } from "$lib";
+
+    export let title: string;
+    export let description: string;
+    export let total: number;
+    export let active: number;
+    export let inactive: number;
+    export let pending: number;
+</script>
+
+<DataTable>
+    <Head>
+        <Row>
+            <CellWithInfo {title} {description} />
+            <Cell>Saldo</Cell>
+        </Row>
+    </Head>
+    <Body>
+        <Row>
+            <CellWithInfo title="⚡ Total" description="Capacidade total" />
+            <Cell numeric>{formatSats(total)}</Cell>
+        </Row>
+        <Row>
+            <CellWithInfo
+                title="✅ Ativo"
+                description="Capacidade disponível par online/operante"
+            />
+            <Cell numeric>{formatSats(active)}</Cell>
+        </Row>
+        <Row>
+            <CellWithInfo
+                title="💤 Inativo"
+                description="Capacidade indisponível par offline/inoperante"
+            />
+            <Cell numeric>{formatSats(inactive)}</Cell>
+        </Row>
+        <Row>
+            <CellWithInfo
+                title="⏳ Pendente"
+                description="Capacidade com transação de abertura não confirmada"
+            />
+            <Cell numeric>{formatSats(pending)}</Cell>
+        </Row>
+    </Body>
+</DataTable>
