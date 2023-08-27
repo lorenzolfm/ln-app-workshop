5339183b478fa544a3b6601b221cb9ed314450da
Lorenzo Maturano <maturanolorenzo@gmail.com>
2023-08-27 19:14:01 -0300

    feat: allowing cors

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 import { env } from "./env";
 import { routes } from "./routes";
 
 const app = express();
 
+app.use(cors());
 routes(app);
 
 app.listen(env.port, () => console.log(`listening on ${env.port}`));
